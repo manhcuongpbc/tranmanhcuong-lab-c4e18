a7d024d759741397be42683d6ac238d47dc556a5
manhcuongpbc <manhcuongpbc2@gmail.com>
2018-06-14 17:46:58 +0700

    ok, check a vs nhe:

--- Lab02/ex2.py
+++ Lab02/ex2.py
@@ -9,47 +9,43 @@ url = "http://s.cafef.vn/bao-cao-tai-chinh/VNM/IncSta/2017/3/0/0/ket-qua-hoat-do
 
 #1.2
 html_content = urlopen(url).read().decode('utf8')
-new_html_content = html_content.replace("<tbody","xxx",3)
+new_html_content = html_content.replace("<table","xxx",3)
 
+# print(new_html_content)
 # 2. Extract ROI (region of interest)
 
 soup = BeautifulSoup(new_html_content, "html.parser")
-# print(soup.prettify())
 
-ul = soup.find("tbody")
-
-print(ul.prettify())
-
-# li_list = ul.find_all("li")
-# print(li_list)
+ul = soup.find("table")
 
 
-# a_list_of_dict = []
-# list_of_query = []
-# for li in li_list:
-#     dict_ = {}
-#     h4 = li.h4
-#     a1 = h4.a
-#     a2 = li.h3.a
-#     name_of_song = a2.string
-#     author = a1.string
-#     dict_["author"] = author
-#     dict_["name of song"] = name_of_song
-#     a_list_of_dict.append(dict_)
-#     list_of_query.append(name_of_song +" " + author)
-#     # print(list_of_query)
+tr_list = ul.find_all("tr")
 
-# pyexcel.save_as(records = a_list_of_dict, dest_file_name = "song_author.xlsx")
 
-# # Sample 4: Search and then download the first video
-# options = {
-#     'default_search': 'ytsearch', # tell downloader to search instead of directly downloading
-#     'max_downloads': 1 # Tell downloader to download only the first entry (video)
-# }
+list_of_td = []
 
-# for i in range(len(list_of_query)):
-#     dl = YoutubeDL(options)
-#     dl.download(list_of_query[i])
+for tr in tr_list:
+    td_list = tr.find_all("td")
+    for td in td_list:
+        k=td.string
+        list_of_td.append(k)
 
-# # 3. Extract info
+# print(new_list_of_td)
 
+
+a_list_of_dict = []
+
+for i in range(0,len(list_of_td),20):
+    dictionary = {}
+
+    dictionary['title'] = list_of_td[i]
+    dictionary['Quy 4 - 2016'] = list_of_td[i+1]
+    dictionary['Quy 1 - 2017'] = list_of_td[i+2]
+    dictionary['Quy 2 - 2017'] = list_of_td[i+3]
+    dictionary['Quy 3 - 2017'] = list_of_td[i+4]
+
+    a_list_of_dict.append(dictionary)
+
+print(a_list_of_dict)
+
+pyexcel.save_as(records = a_list_of_dict, dest_file_name = "vinamilk.xlsx")
